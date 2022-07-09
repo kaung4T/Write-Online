45347d776fe241e4cf50976cd3e946fb1069eb19
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-07-10 00:04:13 +0630

    updateWrite

--- display/models.py
+++ display/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -8,7 +9,11 @@ class UserProfile(AbstractUser):
 
 
 class Write(models.Model):
+    user = models.ForeignKey(UserProfile, default=None, on_delete=models.CASCADE)
     title = models.CharField(max_length=225)
     link = models.URLField(null=True, blank=True)
     description = models.TextField(max_length=4000)
     time = models.TimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}"

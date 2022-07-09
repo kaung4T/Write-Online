730364aa116d1d8dd51a09c241e8fc00e6947d6f
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-07-09 21:26:11 +0630

    updateRest

--- writeOnline/urls.py
+++ writeOnline/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("display.url")),
+    path("rest", include("rest.url")),
 ]
 
 if settings.DEBUG:

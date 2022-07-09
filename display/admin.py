cc59616b48973f9b7e7d27f46606984664aa47a7
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-07-09 21:20:56 +0630

    addWrite

--- display/admin.py
+++ display/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from display.models import UserProfile
+from display.models import UserProfile, Write
+from display.models import Write
 
 # Register your models here.
 # class UserAdmin(admin.ModelAdmin):
 #     list_display = ["image"]
 
+class UserWrite(admin.ModelAdmin):
+    list_display = ["title", "description", "time"]
+
 admin.site.register(UserProfile)
+admin.site.register(Write, UserWrite)
--- display/models.py
+++ display/models.py
@@ -5,3 +5,11 @@ from django.contrib.auth.models import AbstractUser
 
 class UserProfile(AbstractUser):
     image = models.ImageField(null=True, blank=True, upload_to="profile")
+
+
+
+class Write(models.Model):
+    title = models.CharField(max_length=225)
+    link = models.URLField(null=True, blank=True)
+    description = models.TextField(max_length=4000)
+    time = models.TimeField(auto_now_add=True)

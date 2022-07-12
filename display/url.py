9d709cd8c9b49efdf82fb18574385b679c7aa4b4
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-07-12 22:40:36 +0630

    update

--- display/url.py
+++ display/url.py
@@ -3,4 +3,7 @@ from display import views
 
 urlpatterns = [
     path("", views.home, name="index"),
+    path("registration", views.registration, name="registration"),
+    path("login", views.login, name="login"),
+    path("logout", views.logout, name="logout"),
 ]
--- display/views.py
+++ display/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "index.html")
+
+
+def registration(request):
+    return render(request, "registration.html")
+
+
+def login(request):
+    return render(request, "login.html")
+
+
+def logout(request):
+    return render(request, "logout.html")

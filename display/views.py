79a6d421a6d045436bc86d37afee36f3f3bf7253
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-07-13 14:41:58 +0630

    update

--- display/views.py
+++ display/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 def home(request):

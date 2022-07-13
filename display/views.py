31cc07a63c13f639b1ee29f6f936e321b8eec7ff
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-07-14 00:47:17 +0630

    update

--- display/views.py
+++ display/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from display.models import UserProfile
 
 # Create your views here.
 def home(request):
@@ -8,6 +9,29 @@ def home(request):
 
 
 def registration(request):
+    if request.method == "POST":
+        name = request.POST["username"]
+        email = request.POST["email"]
+        password1 = request.POST["password1"]
+        password2 = request.POST["password2"]
+
+        if password1 == password2:
+            if UserProfile.objects.filter(username=name):
+                messages.info(request, "Username already exist!")
+                return redirect("registration")
+
+            if UserProfile.objects.filter(email=email):
+                messages.info(request, "Email already exist!")
+                return redirect("registration")
+
+            UserProfile.objects.create_user(username=name, email=email, password=password1)
+            messages.success(request, "Account created!")
+            return redirect("login")
+
+        else:
+            messages.info(request, "Passwords are not same!")
+            return redirect("registration")
+
     return render(request, "registration.html")
 
 

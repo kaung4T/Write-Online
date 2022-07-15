1e08e4bf34ce193f6cb2173c87193f2d5baffd06
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-07-16 01:24:17 +0630

    update

--- display/models.py
+++ display/models.py
@@ -10,10 +10,20 @@ class UserProfile(AbstractUser):
 
 class Write(models.Model):
     user = models.ForeignKey(UserProfile, default=None, on_delete=models.CASCADE)
+    folder_page = models.CharField(max_length=225, null=True, blank=True)
     title = models.CharField(max_length=225)
     link = models.URLField(null=True, blank=True)
     description = models.TextField(max_length=4000)
     time = models.TimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}"
+
+
+class Folder(models.Model):
+    user = models.ForeignKey(UserProfile, default=None, on_delete=models.CASCADE)
+    write = models.ForeignKey(Write, default=None, on_delete=models.CASCADE)
+    folder = models.CharField(max_length=225)
 
     def __str__(self):
         return f"{self.user.username}"

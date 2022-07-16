6006c8fc6c6686e7c811ce8a65bfcc4eb15a0a52
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-07-16 21:30:14 +0630

    addModel

--- display/admin.py
+++ display/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
 from display.models import UserProfile, Write
-from display.models import Write
-
+from display.models import Write, Folder
+from display.models import Folder
 # Register your models here.
 # class UserAdmin(admin.ModelAdmin):
 #     list_display = ["image"]
 
-class UserWrite(admin.ModelAdmin):
+
+class WriteAdmin(admin.ModelAdmin):
     list_display = ["title", "description", "time"]
 
+
+class FolderAdmin(admin.ModelAdmin):
+    list_display = ["user", "write", "folder"]
+
+
 admin.site.register(UserProfile)
-admin.site.register(Write, UserWrite)
+admin.site.register(Write, WriteAdmin)
+admin.site.register(Folder, FolderAdmin)
--- display/form.py
+++ display/form.py
@@ -1,8 +1,15 @@
-from display.models import Write
+from display.models import Write, Folder
 from django import forms
+from display.models import Folder
 
 
 class WriteForm(forms.ModelForm):
     class Meta:
         model = Write
         fields = ["title", "link", "description"]
+
+
+class FolderForm(forms.ModelForm):
+    class Meta:
+        model = Folder
+        fields = ["folder"]

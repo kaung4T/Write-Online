11495017faed7d16c9b28992c1b195374bdf6662
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-07-21 01:54:53 +0630

    updateAlertMes

--- display/views.py
+++ display/views.py
@@ -22,19 +22,19 @@ def registration(request):
 
         if password1 == password2:
             if UserProfile.objects.filter(username=name).exists():
-                messages.info(request, "Username already exist!")
+                messages.info(request, "Username already exist")
                 return redirect("registration")
 
             if UserProfile.objects.filter(email=email).exists():
-                messages.info(request, "Email already exist!")
+                messages.info(request, "Email already exist")
                 return redirect("registration")
 
             UserProfile.objects.create_user(username=name, email=email, password=password1)
-            messages.success(request, "Account created!")
+            messages.success(request, "Account created")
             return redirect("login")
 
         else:
-            messages.info(request, "Passwords are not same!")
+            messages.info(request, "Passwords are not same")
             return redirect("registration")
 
     return render(request, "registration.html")
@@ -93,7 +93,7 @@ def write2(request, pk):
             folder.write = w
             folder.save()
 
-            messages.success(request, "Your work has successfully been saved")
+            messages.success(request, "Your work has successfully been saved", extra_tags="success")
             return redirect("/")
 
     folder = Folder.objects.get(id=pk)

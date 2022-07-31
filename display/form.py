c625d4f94a3df9d7a18b26948656e8df27c8a8b1
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-07-31 20:55:27 +0630

    update

--- display/form.py
+++ display/form.py
@@ -6,6 +6,9 @@ from display.models import Folder
 class WriteForm(forms.ModelForm):
     class Meta:
         model = Write
+        widgets = {
+            "description": forms.Textarea(attrs={"style": "background-color:#2e2f30;border-color:#2e2f30;", "class": "form-control", "rows": 20, "cols": 45}),
+        }
         fields = ["title", "link", "description"]
 
 

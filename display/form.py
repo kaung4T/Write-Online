2abf351a06c9fe9d1b394d58b60cd3375fbc225f
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-07-31 21:16:43 +0630

    update

--- display/form.py
+++ display/form.py
@@ -4,15 +4,15 @@ from display.models import Folder
 
 
 class WriteForm(forms.ModelForm):
-    title = forms.CharField(widget=forms.TextInput(attrs={"style": "background-color:#2e2f30;border-color:#2e2f30;", "class": "form-control mb-3"}))
-    link = forms.URLField(required=False, widget=forms.URLInput(attrs={"style": "background-color:#2e2f30;border-color:#2e2f30;", "class": "form-control mb-3"}))
+    title = forms.CharField(widget=forms.TextInput(attrs={"style": "background-color:#2e2f30;border-color:#2e2f30;color:white;", "class": "form-control mb-3"}))
+    link = forms.URLField(required=False, widget=forms.URLInput(attrs={"style": "background-color:#2e2f30;border-color:#2e2f30;color:white;", "class": "form-control mb-3"}))
 
     class Meta:
         model = Write
-        
+
         # will same with models
         widgets = {
-            "description": forms.Textarea(attrs={"style": "background-color:#2e2f30;border-color:#2e2f30;", "class": "form-control mb-3", "rows": 20, "cols": 45}),
+            "description": forms.Textarea(attrs={"style": "background-color:#2e2f30;border-color:#2e2f30;color:white;", "class": "form-control mb-3", "rows": 20, "cols": 45}),
             # "link": forms.URLInput(attrs={"style": "background-color:#2e2f30;border-color:#2e2f30;", "class": "form-control mb-3"})
         }
         fields = ["title", "link", "description"]

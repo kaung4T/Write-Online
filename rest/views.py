e90c65727aefe01b6ed8f38f27b6ce7cde274b87
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-08-07 01:13:24 +0630

    update

--- rest/views.py
+++ rest/views.py
@@ -6,6 +6,23 @@ from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 # Create your views here.
 from django.http import JsonResponse
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['name'] = user.username
+        # ...
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 
 @api_view(["GET"])

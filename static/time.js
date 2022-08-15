e300cc2ef970dc188a73c3c56e0b82a7f0a531d3
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-08-15 22:46:13 +0630

    updateTime

--- static/time.js
+++ static/time.js
@@ -1,4 +1,17 @@
 
 
 
+function displayTime () {
+     let dateTime = new Date();
+     let hour = dateTime.getHours();
+     let minute = dateTime.getMinutes();
+     let second = dateTime.getSeconds();
+     let time = document.getElementById("time");
 
+
+     document.getElementById("hour").innerHTML = `${hour}:`;
+     document.getElementById("minute").innerHTML = `${minute}:`;
+     document.getElementById("second").innerHTML = `${second}`;
+}
+
+setInterval(displayTime, 10);
